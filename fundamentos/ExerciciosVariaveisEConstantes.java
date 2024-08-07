74f5d302e03392af2fefd895f75823aabed5f991
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-08-06 23:12:14 -0300

    Create ExerciciosVariaveisEConstantes.java

---
+++ fundamentos/ExerciciosVariaveisEConstantes.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class ExercicioVariaveisEConstantes {
+	
+	public static void main(String[]args) {
+		// o desafio consiste em receber uma variável em fahrentei e passar
+		//para celsius
+		
+		final double F = 50; 
+		double resultado = 0; 
+	
+		resultado = ((F-32)/1.8);
+		System.out.println("Temperatura em Celsius = " + resultado);
+		
+		
+	}
+
+}
