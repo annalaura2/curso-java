eb307ecd90dd2dfa2839a8b610a96c4fc3339a56
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-08-12 16:20:20 -0300

    Create Inferencias.java

---
+++ fundamentos/Inferencias.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class Inferencia {
+	
+	public static void main(String[]args) {
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 15.4;
+		System.out.println(b);
+		// o var, em java, infere o tipo da variável, sem precisar que você declare
+		
+		
+	}
+
+}
