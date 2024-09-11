63128c0db7e09d1c092e953b97b821e3c828bf73
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-11 19:11:57 -0300

    Update Console.java

--- fundamentos/Console.java
+++ fundamentos/Console.java
@@ -6,10 +6,18 @@ public class Console {
 	
 	public static void main(String[] args) {
 		
-		java.util.Scanner entrada = new Scanner(System.in); 
-		System.out.print("Informe a sua idade:");
-		String idade = entrada.nextLine();
-		System.out.println("\nSua idade é = " + idade);
+		java.util.Scanner a = new Scanner(System.in); 
+		java.util.Scanner b = new Scanner(System.in);
+		
+		
+		System.out.print("Informe a sua idade: ");
+		int idade = a.nextInt();
+		System.out.print("Informe seu nome: ");
+		String nome = b.nextLine();
+		
+		
+		System.out.println("\n\nSua idade é = " + idade);
+		System.out.println("Seu nome é = " + nome);
 		
 	}
 
