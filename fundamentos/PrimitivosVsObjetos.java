55af24ee2956ee694854e1fe5dd7e814fd2a6400
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-11 19:45:08 -0300

    Create PrimitivosVsObjetos.java

---
+++ fundamentos/PrimitivosVsObjetos.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+public class PrimitivoVsObjeto {
+	
+	public static void main(String[] args) {
+		
+		//WRAPPERS - versões objeto dos tipos primitivos
+		
+		Integer  a = 100;// int 
+		Short b = 10000;
+		Byte c = 10; 
+		Long d = 100000000L; 
+		
+		System.out.println(c.byteValue());
+		System.out.println(b * 2);
+				
+		
+		
+	}
+
+}
