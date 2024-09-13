9fa932de0c34306697240ebe43856519f761df2d
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-13 11:39:54 -0300

    Create Aritmeticos.java

---
+++ fundamentos/Aritmeticos.java
@@ -0,0 +1,33 @@
+package Fundamentos;
+
+public class Aritmeticos {
+	
+	public static void main(String[]args) {
+		
+		System.out.print("adição direta: "); 
+		System.out.println(3 + 2);
+		
+		double n1 = 34.50; 
+		int n2 = 20;
+		double resultado = n1 - n2; 
+		System.out.println("Subtração armazenando a variável: " + resultado);
+		
+		int d1 = 4; 
+		int d2 = 2; 
+		int divisao = d1/d2;
+		System.out.println("divisão: " + divisao);
+		
+		double m1 = 50; 
+		double m2 = 6; 
+		double multiplicacao = m1*m2; 
+		System.out.println("multiplicação: " + multiplicacao);
+		
+		int d3 = 6;
+		double d4 = 4;
+		double resto = d3 % d4;
+		System.out.println("resto da divisão: " + resto);
+					
+				
+	}
+
+}
