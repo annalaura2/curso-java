936e735190c05ff417b37671adc57f355aee9819
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-13 10:36:45 -0300

    Create ConversaoDeTiposPrimitivos.java

---
+++ fundamentos/ConversaoDeTiposPrimitivos.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+
+public class ConversaoDeTiposPrimitivos {
+	
+	public static void main(String[]args) {
+		
+		double a = 1;
+		float b = (float) 1.4445456; // conversão explicíta
+		
+		
+		System.out.println("Valor de b: " + b);
+	}
+
+}
