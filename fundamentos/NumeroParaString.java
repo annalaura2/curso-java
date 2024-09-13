44b9401c878ed79d2f36c507661fe5780d4d580b
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-13 10:51:48 -0300

    Create NumeroParaString.java

---
+++ fundamentos/NumeroParaString.java
@@ -0,0 +1,18 @@
+package Fundamentos;
+
+
+public class NumeroParaString {
+
+	public static void main(String[]args) {
+		
+		//Número para string
+		
+		Integer num1 = 10000; 
+		System.out.println(num1.toString(num1).length());
+		
+		int num2 = 200000;
+		System.out.println(Integer.toString(num2).length());
+		
+		
+	}
+}
