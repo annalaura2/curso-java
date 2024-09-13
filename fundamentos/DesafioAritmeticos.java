96d87eaaa8dcebbb3560779b6441a344dcb4de68
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-13 11:54:37 -0300

    Create DesafioAritmeticos.java

---
+++ fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+public class DesafioArimeticos {
+	
+	public static void main(String[]args) {
+		
+		double a = (6*(3+2)); 
+		double a1 = (Math.pow(a, 2))/(3*2);
+		
+		
+		double b = ((1-5)*(2-7))/(2);
+		double b1 = (Math.pow(b, 2));
+	
+		
+		double c =(150-100);
+		double c1 = (Math.pow(c, 3))/(10*10*10);
+		
+		System.out.println("Resultado = " + c1);
+	}
+
+}
